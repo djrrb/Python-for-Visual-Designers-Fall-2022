e0fc2029651e04188ed4c72c280d5c18b5564eec
David Jonathan Ross <david@djr.com>
2022-11-04 11:40:17 -0400

    Update wedgeShade.py

--- session-3/challenges/wedgeShade.py
+++ session-3/challenges/wedgeShade.py
@@ -1,4 +1,7 @@
+myText = 'WORD'
+
 newPage(1200, 350)
+
 
 fontSize(200)
 font('NickelGothicv2-Wide')
@@ -8,6 +11,6 @@ with savedState():
     fill(.7)
     scale(1, .9)
     skew(-10)
-    text('WORD', (0, 0), align="center")
+    text(myText, (0, 0), align="center")
 
-text('WORD', (0, 0), align="center")
+text(myText, (0, 0), align="center")
